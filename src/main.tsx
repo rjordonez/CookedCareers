17e8fe6a1b381892c714f9a597a1c739e7b6edfb
Tavasya Ganpati <tavasyag@gmail.com>
2025-10-21 13:04:10 +0700

    f

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,12 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById("root")!).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+  <ClerkProvider
+    publishableKey={PUBLISHABLE_KEY}
+    afterSignOutUrl="/"
+    signInForceRedirectUrl="/dashboard"
+    signUpForceRedirectUrl="/dashboard"
+  >
     <Provider store={store}>
       <App />
     </Provider>
